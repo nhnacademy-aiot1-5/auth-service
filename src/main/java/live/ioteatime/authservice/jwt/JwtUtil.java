ca6664017e5f4c5f8e2482779c6997ad99dd7e41
dongmin <ldm030475@gmail.com>
2024-03-27 20:02:44 +0900

    feat: jwt 토큰 발급

--- src/main/java/live/ioteatime/authservice/jwt/JwtUtil.java
+++ src/main/java/live/ioteatime/authservice/jwt/JwtUtil.java
@@ -8,13 +8,14 @@ import java.util.Date;
 public class JwtUtil {
     public static String createJwt(String userId, String secret) {
         Date date = new Date();
-
+        //TODO Roles도 clams에 넣기
+        SignatureAlgorithm hs256 = SignatureAlgorithm.HS256;
         return Jwts.builder()
                 .setHeaderParam("type", "jwt")
-                .claim("userId", userId)
+                .setSubject(userId)
                 .setIssuedAt(date)
                 .setExpiration(new Date(System.currentTimeMillis() + 1 * (1000 * 60 * 60 * 24 * 365)))
-                .signWith(SignatureAlgorithm.HS256, secret)
+                .signWith(hs256, secret)
                 .compact();
     }
 }
