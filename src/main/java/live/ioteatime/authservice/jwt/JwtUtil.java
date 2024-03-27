94964908d7b8717cb55b088ee0d2e088265d73bf
dongmin <ldm030475@gmail.com>
2024-03-27 20:45:59 +0900

    feat: jwt 토큰 디코딩
    chore: 공백제거

--- src/main/java/live/ioteatime/authservice/jwt/JwtUtil.java
+++ src/main/java/live/ioteatime/authservice/jwt/JwtUtil.java
@@ -1,21 +1,41 @@
 package live.ioteatime.authservice.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
-
+@Slf4j
+@Component
 public class JwtUtil {
-    public static String createJwt(String userId, String secret) {
+    @Value("${secreet.key}")
+    private String secretValue;
+    public String createJwt(String userId) {
+        SignatureAlgorithm hs256 = SignatureAlgorithm.HS256;
+        log.info(secretValue);
+
         Date date = new Date();
         //TODO Roles도 clams에 넣기
-        SignatureAlgorithm hs256 = SignatureAlgorithm.HS256;
         return Jwts.builder()
                 .setHeaderParam("type", "jwt")
                 .setSubject(userId)
                 .setIssuedAt(date)
                 .setExpiration(new Date(System.currentTimeMillis() + 1 * (1000 * 60 * 60 * 24 * 365)))
-                .signWith(hs256, secret)
+                .signWith(hs256, secretValue)
                 .compact();
+    }
+
+    public String getUserId(String jwt) {
+        if (jwt.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        Jws<Claims> claimsJwts = Jwts.parser()
+                .setSigningKey(secretValue)
+                .build().parseSignedClaims(jwt);
+        return claimsJwts.getBody().getSubject();
     }
 }
