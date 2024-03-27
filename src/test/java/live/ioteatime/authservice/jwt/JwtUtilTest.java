a28e5200dbf2f35e9346f76bbe251dfae3a51697
dongmin <ldm030475@gmail.com>
2024-03-27 20:46:37 +0900

    test: jwt 토큰 발급, 토큰 디코딩

--- src/test/java/live/ioteatime/authservice/jwt/JwtUtilTest.java
+++ src/test/java/live/ioteatime/authservice/jwt/JwtUtilTest.java
@@ -1,23 +1,35 @@
 package live.ioteatime.authservice.jwt;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class JwtUtilTest {
+    private String userId;
+    private String jwt;
+    @Autowired
+    private JwtUtil jwtUtil;
 
-    @Value("${secreet.key}")
-    private String secret;
+    @BeforeEach
+    void setUp() {
+        userId = "test";
+
+        jwt = jwtUtil.createJwt(userId);
+    }
+
     @Test
     void createJwtTest() {
-
-        String userId = "test";
-
-        String jwt = JwtUtil.createJwt(userId, secret);
-
         assertFalse(jwt.isEmpty());
     }
 
+    @Test
+    void getUserIdToJwt() {
+       String actual= jwtUtil.getUserId(jwt);
+        assertTrue("test".equals(actual));
+    }
 }
