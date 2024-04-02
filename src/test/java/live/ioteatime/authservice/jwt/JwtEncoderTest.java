61d360a093f04ed72e679da39e02097d42871322
dongmin <ldm030475@gmail.com>
2024-04-02 15:18:34 +0900

    add : refresh 토큰 생성

--- src/test/java/live/ioteatime/authservice/jwt/JwtEncoderTest.java
+++ src/test/java/live/ioteatime/authservice/jwt/JwtEncoderTest.java
@@ -1,18 +1,25 @@
 package live.ioteatime.authservice.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
+@Slf4j
 class JwtEncoderTest {
     private String userId;
     private String jwt;
+   private String refresh;
     @Autowired
     private JwtEncoder jwtEncoder;
 
@@ -21,16 +28,32 @@ class JwtEncoderTest {
         userId = "aa";
 
         jwt = jwtEncoder.createJwt(userId);
+        refresh = jwtEncoder.createRefresh();
     }
 
     @Test
     void createJwtTest() {
         assertFalse(jwt.isEmpty());
+        Date now = new Date();
+
+        Claims claims = jwtEncoder.getClaim(jwt);
+        Date accessExpiredDate = claims.getExpiration();
+        Date expiredDate = new Date(now.getTime() + 1800000);
+
+        assertEquals(accessExpiredDate.toString(),expiredDate.toString());
+
     }
 
     @Test
-    void getUserIdToJwt() {
-       String actual= jwtEncoder.getUserId(jwt);
-        assertEquals("aa", actual);
+    void testCreateRefresh() {
+        Date now = new Date();
+        assertNotNull(refresh);
+
+        Claims claims = jwtEncoder.getClaim(refresh);
+        Date refreshExpiredDate = claims.getExpiration();
+        Date expiredDate = new Date(now.getTime() + 1209600000);
+
+        assertEquals(refreshExpiredDate.toString(),expiredDate.toString());
     }
+
 }
